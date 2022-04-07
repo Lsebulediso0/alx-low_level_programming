456b2681b868bfc6d3fbbc6e7685bac6c39f4644
Letlhogonolo <Lsebulediso0@gmail.com>
2022-04-07 07:12:53 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Print out a string 
- *
- * Return: 0 (success)
+ *main - entry 
+ *Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print out a string
+ * return: 0 (always)
+ */
+
+int main(void)
+{
+	printf("with proper gramer, but the outcome is a piece of art,\n");
+	return (0);
+}
