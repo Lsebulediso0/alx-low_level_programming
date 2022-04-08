52694f3bca164067e17f31231852ad5066ea3f52
Letlhogonolo <Lsebulediso0@gmail.com>
2022-04-07 20:31:33 -0700

    Task 4. Hallo, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - entry.
-
+ * main - entry.
  * Return: 0 if success.
-
  */
-
 int main(void)
-
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
